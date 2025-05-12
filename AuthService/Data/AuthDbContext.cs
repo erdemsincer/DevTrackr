60e751f1a245e4204dde59ba780a1879d634b933
Erdem <erdemsincer123@icloud.com>
2025-05-12 19:22:48 +0300

    Add project files.

---
+++ AuthService/Data/AuthDbContext.cs
@@ -0,0 +1,13 @@
+﻿using AuthService.Models;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+
+namespace AuthService.Data
+{
+    public class AuthDbContext : DbContext
+    {
+        public AuthDbContext(DbContextOptions<AuthDbContext> options) : base(options) { }
+
+        public DbSet<User> Users { get; set; }
+    }
+}
---
+++ AuthService/Models/User.cs
@@ -0,0 +1,15 @@
+﻿namespace AuthService.Models
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public string GitHubUsername { get; set; }  // GitHub OAuth sonrası gelecek
+        public string Email { get; set; }
+        public string PasswordHash { get; set; }    // Normal login için (opsiyonel)
+        public string FullName { get; set; }
+
+        public string Role { get; set; } = "User";  // İleride admin vs ayrımı için
+        public string RefreshToken { get; set; }
+        public DateTime RefreshTokenExpiryTime { get; set; }
+    }
+}
