8b59d4f616af49636345325ff4242d9ae5b9fe66
Erdem <erdemsincer123@icloud.com>
2025-05-13 00:50:42 +0300

    activity controller

---
+++ ActivityService/Controllers/ActivityController.cs
@@ -0,0 +1,18 @@
+﻿using ActivityService.Services;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ActivityService.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class ActivityController(IActivityService _activityService) : ControllerBase
+    {
+        [HttpGet("{githubUsername}")]
+        public async Task<IActionResult> GetSummary(string githubUsername)
+        {
+            var summary = await _activityService.GetActivitySummaryAsync(githubUsername);
+            return Ok(summary);
+        }
+    }
+}
