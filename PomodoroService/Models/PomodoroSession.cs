9114aa1bbe30a15dda8da6c3420f4c5a600f03f9
Erdem <erdemsincer123@icloud.com>
2025-05-13 01:26:27 +0300

    PomodoroSession ENTİTY

---
+++ PomodoroService/Models/PomodoroSession.cs
@@ -0,0 +1,16 @@
+﻿namespace PomodoroService.Models
+{
+    public class PomodoroSession
+    {
+        public int Id { get; set; }
+        public int UserId { get; set; }
+
+        public DateTime StartTime { get; set; } = DateTime.UtcNow;
+        public DateTime? EndTime { get; set; }
+
+        public int FocusMinutes { get; set; }
+        public int BreakMinutes { get; set; }
+
+        public bool IsCompleted { get; set; } = false;
+    }
+}
