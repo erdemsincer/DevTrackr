532bee554c9ac58ef81ecb9c8b6f37bb2fba22af
Erdem <erdemsincer123@icloud.com>
2025-05-12 22:44:05 +0300

    user dtos service

---
+++ UserService/DTOs/UserDto.cs
@@ -0,0 +1,11 @@
+﻿namespace UserService.DTOs
+{
+    public class UserDto
+    {
+        public string FullName { get; set; }
+        public string Email { get; set; }
+        public string GitHubUsername { get; set; }
+        public string Bio { get; set; }
+        public string Theme { get; set; }
+    }
+}
---
+++ UserService/Interfaces/IUserService.cs
@@ -0,0 +1,10 @@
+﻿using UserService.DTOs;
+
+namespace UserService.Interfaces
+{
+    public interface IUserService
+    {
+        Task<UserDto?> GetProfileAsync(int userId);
+        Task<bool> UpdateProfileAsync(int userId, UpdateUserDto dto);
+    }
+}
