c1f495dcfa725d62ed1ed8f584bcdda094e6a7ff
Erdem <erdemsincer123@icloud.com>
2025-05-12 22:43:39 +0300

    events

---
+++ Events/UserCreatedEvent.cs
@@ -0,0 +1,12 @@
+﻿namespace Events
+{
+    public class UserCreatedEvent
+    {
+        public int Id { get; set; }
+        public string FullName { get; set; }
+        public string Email { get; set; }
+        public string Role { get; set; }
+        public string GitHubUsername { get; set; }
+
+    }
+}
