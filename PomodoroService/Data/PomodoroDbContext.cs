a21597b3180e82069b2841d848fe98c75ebcff8b
Erdem <erdemsincer123@icloud.com>
2025-05-13 01:26:42 +0300

    PomodoroSession CONTEXT

---
+++ PomodoroService/Data/PomodoroDbContext.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.EntityFrameworkCore;
+using PomodoroService.Models;
+using System.Collections.Generic;
+
+namespace PomodoroService.Data
+{
+    public class PomodoroDbContext(DbContextOptions<PomodoroDbContext> options) : DbContext(options)
+    {
+        public DbSet<PomodoroSession> Sessions { get; set; }
+    }
+}
