9b108406bf81bee73643cbab9ab046446692416e
Erdem <erdemsincer123@icloud.com>
2025-05-12 22:43:50 +0300

    consumer

---
+++ UserService/Consumers/UserCreatedConsumer.cs
@@ -0,0 +1,38 @@
+﻿using Events;
+using MassTransit;
+using UserService.Data;
+using UserService.Models;
+
+namespace UserService.Consumers
+{
+    public class UserCreatedConsumer : IConsumer<UserCreatedEvent>
+    {
+        private readonly UserDbContext _context;
+
+        public UserCreatedConsumer(UserDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task Consume(ConsumeContext<UserCreatedEvent> context)
+        {
+            var message = context.Message;
+
+            var exists = _context.Users.Any(u => u.Id == message.Id);
+            if (exists) return;
+
+            var newUser = new User
+            {
+                Id = message.Id,
+                FullName = message.FullName,
+                Email = message.Email,
+                GitHubUsername = message.GitHubUsername, // AuthService vermiyor
+                Bio = "",
+                Theme = "light"
+            };
+
+            _context.Users.Add(newUser);
+            await _context.SaveChangesAsync();
+        }
+    }
+}
