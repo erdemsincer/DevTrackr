d8e62d40d8e73428d2671f87071a2e16a3dfebff
Erdem <erdemsincer123@icloud.com>
2025-05-12 19:54:58 +0300

    user models

---
+++ UserService/Models/User.cs
@@ -0,0 +1,12 @@
+﻿namespace UserService.Models
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public string FullName { get; set; }
+        public string Email { get; set; }
+        public string GitHubUsername { get; set; }
+        public string Bio { get; set; }
+        public string Theme { get; set; } = "light"; // light / dark
+    }
+}
