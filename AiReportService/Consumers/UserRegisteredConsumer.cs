7e5b3f500a587d250da31e6520445d1e964b1ac6
Erdem <erdemsincer123@icloud.com>
2025-05-14 01:27:08 +0300

    user register kuyruk

---
+++ AiReportService/Consumers/UserRegisteredConsumer.cs
@@ -0,0 +1,23 @@
+﻿using AiReportService.Services;
+using Events;
+using MassTransit;
+
+namespace AiReportService.Consumers
+{
+    public class UserRegisteredConsumer : IConsumer<UserRegisteredEvent>
+    {
+        private readonly IAiReportService _aiReportService;
+
+        public UserRegisteredConsumer(IAiReportService aiReportService)
+        {
+            _aiReportService = aiReportService;
+        }
+
+        public async Task Consume(ConsumeContext<UserRegisteredEvent> context)
+        {
+            var userId = context.Message.UserId;
+            Console.WriteLine($"📩 Yeni kullanıcı kaydı alındı: userId = {userId}");
+            await _aiReportService.GenerateReportAsync(userId);
+        }
+    }
+}
