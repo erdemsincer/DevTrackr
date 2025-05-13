e489a1a548505cabe0a9dd6bd9290cebf2a5a4a6
Erdem <erdemsincer123@icloud.com>
2025-05-14 01:27:42 +0300

    generateweekly

---
+++ AiReportService/Jobs/GenerateWeeklyReportsJob.cs
@@ -0,0 +1,27 @@
+﻿using AiReportService.External;
+using AiReportService.Services;
+using Quartz;
+
+namespace AiReportService.Jobs
+{
+    public class GenerateWeeklyReportsJob : IJob
+    {
+        private readonly IUserClient _userClient;
+        private readonly IAiReportService _aiReportService;
+
+        public GenerateWeeklyReportsJob(IUserClient userClient, IAiReportService aiReportService)
+        {
+            _userClient = userClient;
+            _aiReportService = aiReportService;
+        }
+
+        public async Task Execute(IJobExecutionContext context)
+        {
+            var userIds = await _userClient.GetAllUserIdsAsync();
+            foreach (var userId in userIds)
+            {
+                await _aiReportService.GenerateReportAsync(userId);
+            }
+        }
+    }
+}
