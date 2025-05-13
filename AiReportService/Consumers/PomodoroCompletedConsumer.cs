7f3411c2ca2c67a0d5328fbdc301891eaf627aa6
Erdem <erdemsincer123@icloud.com>
2025-05-14 02:08:33 +0300

    görev tamamlandıktan sonra mmessgwe

---
+++ AiReportService/Consumers/PomodoroCompletedConsumer.cs
@@ -0,0 +1,23 @@
+﻿using AiReportService.Services;
+using Events;
+using MassTransit;
+
+namespace AiReportService.Consumers
+{
+    public class PomodoroCompletedConsumer : IConsumer<PomodoroCompletedEvent>
+    {
+        private readonly IAiReportService _aiReportService;
+
+        public PomodoroCompletedConsumer(IAiReportService aiReportService)
+        {
+            _aiReportService = aiReportService;
+        }
+
+        public async Task Consume(ConsumeContext<PomodoroCompletedEvent> context)
+        {
+            var userId = context.Message.UserId;
+            Console.WriteLine($"⏱️ Pomodoro tamamlandı event geldi: userId = {userId}");
+            await _aiReportService.GenerateQuickReportAsync(userId);
+        }
+    }
+}
---
+++ AiReportService/Consumers/TaskCompletedConsumer.cs
@@ -0,0 +1,23 @@
+﻿using AiReportService.Services;
+using Events;
+using MassTransit;
+
+namespace AiReportService.Consumers
+{
+    public class TaskCompletedConsumer : IConsumer<TaskCompletedEvent>
+    {
+        private readonly IAiReportService _aiReportService;
+
+        public TaskCompletedConsumer(IAiReportService aiReportService)
+        {
+            _aiReportService = aiReportService;
+        }
+
+        public async Task Consume(ConsumeContext<TaskCompletedEvent> context)
+        {
+            var userId = context.Message.UserId;
+            Console.WriteLine($"📥 Task tamamlandı event geldi: userId = {userId}");
+            await _aiReportService.GenerateQuickReportAsync(userId);
+        }
+    }
+}
