59550ede288a6eff88ccfee583f5739e4b6f3f22
Erdem <erdemsincer123@icloud.com>
2025-05-13 21:24:57 +0300

    ai contex ayarları

---
+++ AiReportService/Data/AiReportDbContext.cs
@@ -0,0 +1,11 @@
+﻿using AiReportService.Entities;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+
+namespace AiReportService.Data
+{
+    public class AiReportDbContext(DbContextOptions<AiReportDbContext> options) : DbContext(options)
+    {
+        public DbSet<AiReport> Reports => Set<AiReport>();
+    }
+}
