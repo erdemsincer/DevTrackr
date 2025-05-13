3cf5e64001c3c167f11df3b8c6de58a4c4d31a87
Erdem <erdemsincer123@icloud.com>
2025-05-13 21:25:54 +0300

    migrateler

---
+++ AiReportService/Services/IAiReportService.cs
@@ -0,0 +1,9 @@
+﻿using AiReportService.Entities;
+
+namespace AiReportService.Services
+{
+    public interface IAiReportService
+    {
+        Task<AiReport> GenerateReportAsync(int userId);
+    }
+}
