a6f7fe40783c8ce297534e3b81d12e5ce746933e
Erdem <erdemsincer123@icloud.com>
2025-05-13 21:25:17 +0300

    task client

---
+++ AiReportService/External/ITaskClient.cs
@@ -0,0 +1,7 @@
+﻿namespace AiReportService.External
+{
+    public interface ITaskClient
+    {
+        Task<List<string>> GetCompletedTasksAsync(int userId);
+    }
+}
---
+++ AiReportService/External/TaskClient.cs
@@ -0,0 +1,40 @@
+﻿using System.Net.Http.Headers;
+using System.Text.Json;
+
+namespace AiReportService.External
+{
+    public class TaskClient : ITaskClient
+    {
+        private readonly HttpClient _httpClient;
+        private readonly IHttpContextAccessor _httpContextAccessor;
+
+        public TaskClient(HttpClient httpClient, IHttpContextAccessor httpContextAccessor)
+        {
+            _httpClient = httpClient;
+            _httpContextAccessor = httpContextAccessor;
+        }
+
+        public async Task<List<string>> GetCompletedTasksAsync(int userId)
+        {
+            AddJwtFromContext();
+
+            var response = await _httpClient.GetAsync($"/api/Task/{userId}/completed");
+            if (!response.IsSuccessStatusCode) return new List<string>();
+
+            var content = await response.Content.ReadAsStringAsync();
+            return JsonSerializer.Deserialize<List<string>>(content, new JsonSerializerOptions
+            {
+                PropertyNameCaseInsensitive = true
+            }) ?? new List<string>();
+        }
+
+        private void AddJwtFromContext()
+        {
+            var token = _httpContextAccessor.HttpContext?.Request.Headers.Authorization.ToString().Replace("Bearer ", "");
+            if (!string.IsNullOrEmpty(token))
+            {
+                _httpClient.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", token);
+            }
+        }
+    }
+}
