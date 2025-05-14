0d3e3169cee65341e1478182497dc0615223d6fe
Erdem <erdemsincer123@icloud.com>
2025-05-15 01:30:27 +0300

    mail settings

---
+++ AiReportService/Settings/MailSettings.cs
@@ -0,0 +1,11 @@
+﻿namespace AiReportService.Settings
+{
+    public class MailSettings
+    {
+        public string From { get; set; } = string.Empty;
+        public string SmtpHost { get; set; } = string.Empty;
+        public int SmtpPort { get; set; }
+        public string SmtpUser { get; set; } = string.Empty;
+        public string SmtpPass { get; set; } = string.Empty;
+    }
+}
