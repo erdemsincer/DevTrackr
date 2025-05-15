a264f354c624485540e35e37606484a18e07ebd1
Erdem <erdemsincer123@icloud.com>
2025-05-15 19:52:21 +0300

    route ayarları

---
+++ devtrackr-ui/src/routes/index.tsx
@@ -0,0 +1,34 @@
+﻿import { createBrowserRouter } from "react-router-dom";
+import MainLayout from "../layouts/MainLayout";
+import AuthLayout from "../pages/auth/AuthLayout";
+import Login from "../pages/auth/Login";
+import Register from "../pages/auth/Register";
+import Dashboard from "../pages/dashboard/Dashboard";
+import ReportList from "../pages/reports/ReportList";
+import ProtectedRoute from "./ProtectedRoute";
+
+export const router = createBrowserRouter([
+    {
+        path: "/",
+        element: <ProtectedRoute />,
+        children: [
+            {
+                path: "/",
+                element: <MainLayout />,
+                children: [
+                    { index: true, element: <Dashboard /> },
+                    { path: "reports", element: <ReportList /> },
+                ],
+            },
+        ],
+    },
+    {
+        path: "/auth", // 🔥 değiştirilen path
+        element: <AuthLayout />,
+        children: [
+            { path: "login", element: <Login /> },
+            { path: "register", element: <Register /> },
+        ],
+    },
+]);
+
