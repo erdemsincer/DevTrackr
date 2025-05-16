bbe4e69e8f821192fe4df49532c544dfca5a9f3f
Erdem <erdemsincer123@icloud.com>
2025-05-16 03:03:18 +0300

    dashboard sayfası

--- devtrackr-ui/src/pages/dashboard/Dashboard.tsx
+++ devtrackr-ui/src/pages/dashboard/Dashboard.tsx
@@ -1,7 +1,23 @@
+import ProfileCard from "./ProfileCard";
+import AiReportCard from "./AiReportCard";
+import GithubCard from "./GithubCard";
+import TaskCard from "./TaskCard";
+import PomodoroCard from "./PomodoroCard";
+import StatsCard from "./StatsCard";
+
 import "./Dashboard.css";
 
 const Dashboard = () => {
-    return <h1 className="page-title">Dashboard Sayfasý</h1>;
+    return (
+        <div className="dashboard-grid">
+            <ProfileCard />
+            <AiReportCard />
+            <GithubCard />
+            <TaskCard />
+            <PomodoroCard />
+            <StatsCard />
+        </div>
+    );
 };
 
 export default Dashboard;
